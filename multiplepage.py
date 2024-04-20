f5cf5c21714d4900853d23d300e5fb37575e29a3
Bahauddin Habibullah <84557850+bahau88@users.noreply.github.com>
2024-04-20 20:20:26 +0200

    Update multiplepage.py

--- multiplepage.py
+++ multiplepage.py
@@ -22,7 +22,7 @@ with st.sidebar:
     styles={
         #"container": {"padding": "0!important", "background-color": "#fafafa"},
         #"icon": {"color": "orange", "font-size": "25px"}, 
-        "nav-link": {"font-size": "25px", "font-family": "montserrat", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
         "nav-link-selected": {"background-color": "#9478ff"},
     }
 )
