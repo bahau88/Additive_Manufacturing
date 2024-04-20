df578012e7ebe0281cad689c6662a72f6ff96c7b
Bahauddin Habibullah <84557850+bahau88@users.noreply.github.com>
2024-04-20 19:37:10 +0200

    Update multiplepage.py

--- multiplepage.py
+++ multiplepage.py
@@ -1,67 +1,24 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import snowflake.connector
-import streamlit_option_menu
-from streamlit_option_menu import option_menu
 
-with st.sidebar:
-    selected = option_menu(
-    menu_title = "IRT Saint Exupery",
-    options = ["Home","Warehouse","Query Optimization and Processing","Storage","Contact Us", "Contact Us", "Contact Us"],
-    #icons = ["house","gear","activity","snowflake","envelope"],
-    menu_icon = "cast",
-    default_index = 0,
-    #orientation = "horizontal",
-)
-if selected == "Home":
-    st.header('Snowflake Healthcare App')
-    # Create a row layout
-    c1, c2= st.columns(2)
-    c3, c4= st.columns(2)
+def home():
+    st.write("Welcome to the Home page!")
+    # Add your home page content here
 
-    with st.container():
-        c1.write("c1")
-        c2.write("c2")
+def about():
+    st.write("This is the About page.")
+    # Add your about page content here
 
-    with st.container():
-        c3.write("c3")
-        c4.write("c4")
+def contact():
+    st.write("You're on the Contact page.")
+    # Add your contact page content here
 
-    with c1:
-        chart_data = pd.DataFrame(np.random.randn(20, 3),columns=['a', 'b', 'c'])
-        st.area_chart(chart_data)
-           
-    with c2:
-        chart_data = pd.DataFrame(np.random.randn(20, 3),columns=["a", "b", "c"])
-        st.bar_chart(chart_data)
+# Sidebar navigation
+nav_selection = st.sidebar.radio("Navigation", ["Home", "About", "Contact"])
 
-    with c3:
-        chart_data = pd.DataFrame(np.random.randn(20, 3),columns=['a', 'b', 'c'])
-        st.line_chart(chart_data)
-
-    with c4:
-        chart_data = pd.DataFrame(np.random.randn(20, 3),columns=['a', 'b', 'c'])
-        st.line_chart(chart_data)
-        
-    
-if selected == "Warehouse":
-    st.subheader(f"**You Have selected {selected}**")
-    my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-    my_cur = my_cnx.cursor()
-    # run a snowflake query and put it all in a var called my_catalog
-    my_cur.execute("select * from SWEATSUITS")
-    my_catalog = my_cur.fetchall()
-    st.dataframe(my_catalog)
-    q1 = st.text_input('Write your query','')
-    st.button('Run Query')
-    if not q1:
-      st.error('Please write a query')
-    else:
-      my_cur.execute(q1)
-      my_catalog = my_cur.fetchall()
-      st.dataframe(my_catalog)
-
-    
-if selected == "Contact":
-    st.subheader(f"**You Have selected {selected}**")
+# Main content based on sidebar selection
+if nav_selection == "Home":
+    home()
+elif nav_selection == "About":
+    about()
+elif nav_selection == "Contact":
+    contact()
